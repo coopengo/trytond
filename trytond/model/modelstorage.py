d4b8b93bd0753915eb88a46768b5f195732309cc
Jean CAVALLO <jean.cavallo@coopengo.com>
2014-01-31 10:56:23 +0100

    Improve Selection fields Debugging [CUSTOM]

--- trytond/model/modelstorage.py
+++ trytond/model/modelstorage.py
@@ -5,6 +5,7 @@ import base64
 import csv
 import datetime
 import decimal
+import logging
 import random
 import time
 from collections import defaultdict
@@ -1464,6 +1465,11 @@ class ModelStorage(Model):
                             values = value or []
                         for value in values:
                             if value not in test:
+                                logging.getLogger().debug(
+                                    'Bad Selection : field %s of model %s :'
+                                    ' %s is not in %s' % (
+                                        field_name, cls.__name__, value,
+                                        test))
                                 error_args = cls.__names__(field_name)
                                 error_args['value'] = value
                                 raise SelectionValidationError(gettext(
