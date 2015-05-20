1bf291ef1c2400b963bfd2334eddae01a72cf167
Frederic Langevin <frederic.langevin@coopengo.com>
2015-05-20 16:42:03 +0200

    Improve sync_all script

--- sync_all.sh
+++ sync_all.sh
@@ -1,89 +1,51 @@
-cp sync_all.sh /tmp
+SEP=------------------------------------------------
+
+EXPECTED_ARGS=1
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo $SEP
+    echo Usage:
+    echo First argument : tag or branch to sync
+    echo $SEP
+    exit 0
+fi
+
 hg update default
 hg pull http://hg.tryton.org/trytond
 cd trytond/modules
 cp __init__.py ../tmp__init__
 rm -r *
 
-hg clone http://hg.tryton.org/modules/account
-rm -r account/.hg
-rm -r account/.hgtags
-
-hg clone http://hg.tryton.org/modules/account_invoice
-rm -r account_invoice/.hg
-rm -r account_invoice/.hgtags
-
-hg clone http://hg.tryton.org/modules/account_payment
-rm -r account_payment/.hg
-rm -r account_payment/.hgtags
-
-hg clone http://hg.tryton.org/modules/account_payment_sepa
-rm -r account_payment_sepa/.hg
-rm -r account_payment_sepa/.hgtags
-
-hg clone http://hg.tryton.org/modules/account_payment_sepa_cfonb
-rm -r account_payment_sepa_cfonb/.hg
-rm -r account_payment_sepa_cfonb/.hgtags
-
-hg clone http://hg.tryton.org/modules/account_payment_clearing
-rm -r account_payment_clearing/.hg
-rm -r account_payment_clearing/.hgtags
-
-hg clone http://hg.tryton.org/modules/account_product
-rm -r account_product/.hg
-rm -r account_product/.hgtags
-
-hg clone http://hg.tryton.org/modules/account_statement
-rm -r account_statement/.hg
-rm -r account_statement/.hgtags
-
-hg clone http://hg.tryton.org/modules/account_dunning
-rm -r account_dunning/.hg
-rm -r account_dunning/.hgtags
-
-hg clone http://hg.tryton.org/modules/account_dunning_letter
-rm -r account_dunning_letter/.hg
-rm -r account_dunning_letter/.hgtags
-
-hg clone http://hg.tryton.org/modules/bank
-rm -r bank/.hg
-rm -r bank/.hgtags
-
-hg clone http://hg.tryton.org/modules/company
-rm -r company/.hg
-rm -r company/.hgtags
-
-hg clone http://hg.tryton.org/modules/country
-rm -r country/.hg
-rm -r country/.hgtags
-
-hg clone http://hg.tryton.org/modules/currency
-rm -r currency/.hg
-rm -r currency/.hgtags
-
-hg clone http://hg.tryton.org/modules/party
-rm -r party/.hg
-rm -r party/.hgtags
-
-hg clone http://hg.tryton.org/modules/party_relationship
-rm -r party_relationship/.hg
-rm -r party_relationship/.hgtags
-
-hg clone http://hg.tryton.org/modules/party_siret
-rm -r party_siret/.hg
-rm -r party_siret/.hgtags
-
-hg clone http://hg.tryton.org/modules/product
-rm -r product/.hg
-rm -r product/.hgtags
-
-hg clone http://hg.tryton.org/modules/commission
-rm -r commission/.hg
-rm -r commission/.hgtags
-
-hg clone http://hg.tryton.org/modules/commission_waiting
-rm -r commission_waiting/.hg
-rm -r commission_waiting/.hgtags
+declare -a modules=(
+    "account"
+    "account_invoice"
+    "account_dunning"
+    "account_dunning_letter"
+    "account_payment"
+    "account_payment_sepa"
+    "account_payment_sepa_cfonb"
+    "account_payment_clearing"
+    "account_product"
+    "account_statement"
+    "bank"
+    "company"
+    "country"
+    "currency"
+    "party"
+    "party_relationship"
+    "party_siret"
+    "product"
+    "commission"
+    "commission_waiting"
+    )
+for module in "${modules[@]}"
+    do echo "$module"
+    hg clone http://hg.tryton.org/modules/"$module"
+    cd "$module"
+    hg update "$1"
+    rm -r .hg
+    rm -r .hgtags
+    cd ..
+done
 
 mv ../tmp__init__ __init__.py
-rm /tmp/sync_all.sh
