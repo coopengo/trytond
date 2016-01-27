fd226275ec77a324cef377944b7fca872ed488d6
Ali Kefia <ali.kefia@coopengo.com>
2016-01-27 11:46:24 +0100

    same namespacing rule for env vars

--- trytond/config_coog.py
+++ trytond/config_coog.py
@@ -4,9 +4,9 @@ from trytond.config import config
 
 def get_cache_redis():
     return config.get('cache', 'redis',
-        default=os.environ.get('COOG_CACHE_REDIS'))
+        default=os.environ.get('TRYTOND_CACHE_REDIS'))
 
 
 def get_sentry_dsn():
     return config.get('sentry', 'dsn',
-        default=os.environ.get('COOG_SENTRY_DSN'))
+        default=os.environ.get('TRYTOND_SENTRY_DSN'))
