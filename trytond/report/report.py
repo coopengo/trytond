coopengo/trytond

commit 177a6a96efeaa212c1705f9c1d9fbfa7fbac702d

unoconv: call in no-launch mode: fix #2188

trytond/report/report.py
@@ -246,12 +246,13 @@ class Report(URLMixin, PoolBase):
         oext = FORMAT2EXT.get(output_format, output_format)
         with os.fdopen(fd, 'wb+') as fp:
             fp.write(data)
-        cmd = ['unoconv', '--connection=%s' % config.get('report', 'unoconv'),
-            '-f', oext, '--stdout', path]
+        cmd = ['unoconv', '--no-launch', '--connection=%s' % config.get(
+                'report', 'unoconv'), '-f', oext, '--stdout', path]
         try:
-            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-            stdoutdata, stderrdata = proc.communicate()
+            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE, close_fds=True)
             if proc.wait() != 0:
+                stdoutdata, stderrdata = proc.communicate()
                 raise Exception(stderrdata)
             return oext, stdoutdata
         finally:

commit 48b7450c03fbc7c198d0edd9e0447ae33d2e290e

unoconv: communicate in all cases (ok for stdout, ko for stderr): fix #2188 regression introduced by 177a6a96

trytond/report/report.py
@@ -251,8 +251,9 @@ class Report(URLMixin, PoolBase):
         try:
             proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE, close_fds=True)
-            if proc.wait() != 0:
-                stdoutdata, stderrdata = proc.communicate()
+            ret = proc.wait()
+            stdoutdata, stderrdata = proc.communicate()
+            if ret != 0:
                 raise Exception(stderrdata)
             return oext, stdoutdata
         finally: