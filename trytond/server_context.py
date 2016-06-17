b3b68e29b9afd3101ce6a5e367d1e96231d09444
Jean Cavallo <jean.cavallo@coopengo.com>
2016-06-17 16:20:35 +0200

    trytond : Add server_context
    Fix #4248
    review http://rietveld.coopengo.com/26570001

---
+++ trytond/server_context.py
@@ -0,0 +1,61 @@
+import logging
+from threading import local
+
+
+__all__ = [
+    'ServerContext',
+    ]
+
+
+class _AttributeManager(object):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def __enter__(self):
+        return ServerContext()
+
+    def __exit__(self, type, value, traceback):
+        context = ServerContext()
+        for name, value in self.kwargs.iteritems():
+            setattr(context, name, value)
+
+
+class _Local(local):
+    instance = None
+
+
+class ServerContext(object):
+    'Trytond context controller'
+    _local = _Local()
+
+    def __new__(cls):
+        instance = cls._local.instance
+        if not instance:
+            cls._local.instance = super(ServerContext, cls).__new__(cls)
+            logging.getLogger().debug('New Server Context instance created')
+            cls._local.instance.context = {}
+        return cls._local.instance
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._local.instance = None
+
+    def get(self, *args, **kwargs):
+        return self.context.get(*args, **kwargs)
+
+    def set_context(self, context=None, **kwargs):
+        if context is None:
+            context = {}
+        manager = _AttributeManager(context=self.context)
+        self.context = self.context.copy()
+        self.context.update(context)
+        if kwargs:
+            self.context.update(kwargs)
+        return manager
+
+    def reset_context(self):
+        manager = _AttributeManager(context=self.context)
+        self.context = {}
+        return manager
---
+++ trytond/tests/test_server_context.py
@@ -0,0 +1,34 @@
+import unittest
+
+from trytond.server_context import ServerContext
+
+
+class ServerContextTestCase(unittest.TestCase):
+    'Test Server Context'
+
+    def test_server_context(self):
+        with ServerContext().set_context(a=1):
+            self.assertEqual(ServerContext().context.get('a'), 1)
+            self.assertEqual(ServerContext().context.get('b', 10), 10)
+            self.assertEqual(ServerContext().get('a'), 1)
+            self.assertEqual(ServerContext().get('b', 10), 10)
+
+            with ServerContext().set_context(a=10):
+                self.assertEqual(ServerContext().context.get('a'), 10)
+            self.assertEqual(ServerContext().context.get('a'), 1)
+
+            with ServerContext().set_context(c=10):
+                self.assertEqual(ServerContext().context.get('a'), 1)
+                self.assertEqual(ServerContext().context.get('c'), 10)
+            self.assertEqual(ServerContext().context.get('c', 1), 1)
+
+            self.assertEqual(ServerContext().context, {'a': 1})
+        self.assertEqual(ServerContext().context, {})
+
+
+def suite():
+    func = unittest.TestLoader().loadTestsFromTestCase
+    suite = unittest.TestSuite()
+    for testcase in (ServerContextTestCase,):
+        suite.addTests(func(testcase))
+    return suite
