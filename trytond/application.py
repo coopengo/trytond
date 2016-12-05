a725d26d51c9aa462c772df1f96beef9847131c9
Ali Kefia <ali.kefia@coopengo.com>
2016-12-05 15:51:11 +0100

    docker: python app logging to logfile on wsgi middleware Fix #5269

--- trytond/application.py
+++ trytond/application.py
@@ -1,5 +1,14 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+import os
+import logging
+
+log_file = os.environ.get('WSGI_LOG_FILE')
+if log_file:
+    log_level = os.environ.get('LOG_LEVEL', 'ERROR')
+    logging.basicConfig(level=getattr(logging, log_level),
+        filename=log_file)
+
 from trytond.pool import Pool
 from trytond.wsgi import app
 
