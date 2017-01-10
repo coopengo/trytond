42ffa4e40c5e1b9815aeaba74c0d7837ca37a755
Jean Cavallo <jean.cavallo@coopengo.com>
2017-01-10 15:07:37 +0100

    Fix cached db tests for scenarii

--- trytond/tests/tools.py
+++ trytond/tests/tools.py
@@ -2,17 +2,18 @@
 # this repository contains the full copyright notices and license terms.
 from proteus import config, Model, Wizard
 
-from .test_tryton import restore_db_cache, backup_db_cache, drop_create
+from .test_tryton import restore_db_cache, backup_db_cache, drop_db, create_db
 
 
 def activate_modules(modules):
     if isinstance(modules, basestring):
         modules = [modules]
     cache_name = '-'.join(modules)
+    # JCA : restore_db_cache fails if the database already exists
+    drop_db()
     if restore_db_cache(cache_name):
         return _get_config()
-    drop_create()
-
+    create_db()
     cfg = _get_config()
     Module = Model.get('ir.module')
     modules = Module.find([
