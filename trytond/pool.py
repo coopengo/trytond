667f76f15fdbeecc6bfb6c5717e19ce2df582d1d
Jean Cavallo <jean.cavallo@hotmail.fr>
2017-03-23 10:51:16 +0100

    Trigger post_init after setup [CUSTOM]
    Fix #5
    
    Post init hooks should be triggered at the end of Pool.setup in order to be
    sure they are properly called when upgrading the database.

--- trytond/pool.py
+++ trytond/pool.py
@@ -106,7 +106,9 @@ class Pool(object):
 
     @staticmethod
     def register_post_init_hooks(*hooks, **kwargs):
-        Pool._init_hooks[kwargs['module']] = hooks
+        if kwargs['module'] not in Pool._init_hooks:
+            Pool._init_hooks[kwargs['module']] = []
+        Pool._init_hooks[kwargs['module']] += hooks
 
     @classmethod
     def start(cls):
@@ -186,7 +188,6 @@ class Pool(object):
                 raise
             if restart:
                 self.init()
-            self.post_init()
 
     def post_init(self):
         for hook in self._post_init_calls[self.database_name]:
@@ -275,6 +276,7 @@ class Pool(object):
                 cls.__setup__()
             for cls in lst:
                 cls.__post_setup__()
+        self.post_init()
 
     def setup_mixin(self, modules, type=None, name=None):
         logger.info('setup mixin for "%s"', self.database_name)
