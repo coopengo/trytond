48b1dfa18ad1c38a6e78b25ace40b6a98f3c35f1
Jean Cavallo <jean.cavallo@coopengo.com>
2017-08-30 18:15:13 +0200

    Always set log level for tracebacks [CUSTOM]

--- trytond/protocols/dispatcher.py
+++ trytond/protocols/dispatcher.py
@@ -9,6 +9,7 @@ try:
     from http import HTTPStatus
 except ImportError:
     from http import client as HTTPStatus
+import traceback
 
 from sql import Table
 from werkzeug.exceptions import abort
@@ -34,6 +35,13 @@ ir_configuration = Table('ir_configuration')
 ir_lang = Table('ir_lang')
 ir_module = Table('ir_module')
 res_user = Table('res_user')
+
+
+def log_exception(method, *args, **kwargs):
+    kwargs['exc_info'] = False
+    method(*args, **kwargs)
+    for elem in traceback.format_exc().split('\n'):
+        method(elem)
 
 
 @app.route('/<string:database_name>/', methods=['POST'])
@@ -204,20 +212,20 @@ def _dispatch(request, pool, *args, **kwargs):
                 if log_threshold != -1:
                     log_end = time.time()
                     log_args += (str(log_end - log_start),)
-                logger.error(log_message, *log_args, exc_info=True)
+                log_exception(logger.error, log_message, *log_args)
                 raise
             except (ConcurrencyException, UserError, UserWarning,
                     LoginException):
                 if log_threshold != -1:
                     log_end = time.time()
                     log_args += (str(log_end - log_start),)
-                logger.debug(log_message, *log_args, exc_info=True)
+                log_exception(logger.debug, log_message, *log_args)
                 raise
             except Exception:
                 if log_threshold != -1:
                     log_end = time.time()
                     log_args += (str(log_end - log_start),)
-                logger.error(log_message, *log_args, exc_info=True)
+                log_exception(logger.error, log_message, *log_args)
                 raise
             # Need to commit to unlock SQLite database
             transaction.commit()
