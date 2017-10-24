ce446805800939719983e50a5630d3b84140f74b
Ali Kefia <alikefia@users.noreply.github.com>
2017-10-24 10:24:22 +0200

    logging: use default logging format when using wsgi
    Fix #7381

--- trytond/application.py
+++ trytond/application.py
@@ -3,11 +3,13 @@
 import os
 import logging
 
+LF = '%(process)s %(thread)s [%(asctime)s] %(levelname)s %(name)s %(message)s'
+
 log_file = os.environ.get('WSGI_LOG_FILE')
 if log_file:
     log_level = os.environ.get('LOG_LEVEL', 'ERROR')
     logging.basicConfig(level=getattr(logging, log_level),
-        filename=log_file)
+        format=LF, filename=log_file)
 
 from trytond.pool import Pool
 from trytond.wsgi import app
