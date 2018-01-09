141604a030c02e121a5e3db960fbe137ce8957c2
Ali Kefia <ali.kefia@coopengo.com>
2018-01-09 16:35:13 +0100

    merge trytond/application

--- trytond/application.py
+++ trytond/application.py
@@ -1,35 +1,24 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-<<<<<<< HEAD
-import os
-import logging
-
-LF = '%(process)s %(thread)s [%(asctime)s] %(levelname)s %(name)s %(message)s'
-
-log_file = os.environ.get('WSGI_LOG_FILE')
-if log_file:
-    log_level = os.environ.get('LOG_LEVEL', 'ERROR')
-    logging.basicConfig(level=getattr(logging, log_level),
-        format=LF, filename=log_file)
-
-from trytond.pool import Pool
-from trytond.wsgi import app
-
-__all__ = ['app']
-
-import trytond.protocols.dispatcher
-=======
 import csv
 import os
-import logging.config
+import logging
 from io import StringIO
 
 __all__ = ['app']
 
-# Logging must be set before importing
-logging_config = os.environ.get('TRYTOND_LOGGING_CONFIG')
-if logging_config:
-    logging.config.fileConfig(logging_config)
+LF = '%(process)s %(thread)s [%(asctime)s] %(levelname)s %(name)s %(message)s'
+log_file = os.environ.get('WSGI_LOG_FILE')
+if log_file:
+    log_level = os.environ.get('LOG_LEVEL', 'ERROR')
+    logging.basicConfig(level=getattr(logging, log_level), format=LF,
+        filename=log_file)
+
+if not log_file:
+    # Logging must be set before importing
+    logging_config = os.environ.get('TRYTOND_LOGGING_CONFIG')
+    if logging_config:
+        logging.config.fileConfig(logging_config)
 
 from trytond.pool import Pool
 from trytond.wsgi import app
@@ -42,4 +31,3 @@ if db_names:
     reader = csv.reader(StringIO(unicode(db_names)))
     for db_name in next(reader):
         Pool(db_name).init()
->>>>>>> 4.6
