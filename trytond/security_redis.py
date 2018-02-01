e4cd66d39631c11c7a55ee561944b914100e673f
Ali Kefia <ali.kefia@coopengo.com>
2018-02-01 16:54:57 +0100

    copyright

--- trytond/security_redis.py
+++ trytond/security_redis.py
@@ -1,3 +1,5 @@
+# This file is part of Coog. The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 # AKE: manage session on redis
 from threading import Lock
 import redis
