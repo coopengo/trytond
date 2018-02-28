36c64d8ab953e57f325cb649668d06f994b2eeb0
Nicolas Évrard <nicoe@users.noreply.github.com>
2018-02-28 14:53:45 +0100

    Add form_toolbar keyword [CUSTOM]
    PR: #31
    
    Fix #7934
    PR relative à https://support.coopengo.com/issues/7934

--- trytond/model/modelview.py
+++ trytond/model/modelview.py
@@ -301,6 +301,7 @@ class ModelView(Model):
         prints = Action.get_keyword('form_print', (cls.__name__, -1))
         actions = Action.get_keyword('form_action', (cls.__name__, -1))
         relates = Action.get_keyword('form_relate', (cls.__name__, -1))
+        quick_actions = Action.get_keyword('form_toolbar', (cls.__name__, -1))
         exports = Export.search_read(
             [('resource', '=', cls.__name__)],
             fields_names=['name', 'export_fields.name'])
@@ -311,6 +312,7 @@ class ModelView(Model):
             'print': prints,
             'action': actions,
             'relate': relates,
+            'quick_actions': quick_actions,
             'exports': exports,
             'emails': emails,
             }
