7852abdf983a7ec2bcb905d84e363a5e3fe143e3
Jean Cavallo <jean.cavallo@hotmail.fr>
2018-06-19 13:57:34 +0200

    Remove activate_module patch

--- trytond/tests/test_tryton.py
+++ trytond/tests/test_tryton.py
@@ -46,14 +46,11 @@ DB_CACHE = os.environ.get('DB_CACHE')
 Pool.test = True
 
 
-# JCA : Allow to force filename in case it is too long
-def activate_module(name, cache_file_name=None):
+def activate_module(name):
     '''
     Activate module for the tested database
     '''
-    if cache_file_name is None:
-        cache_file_name = name
-    if not db_exist(DB_NAME) and restore_db_cache(cache_file_name):
+    if not db_exist(DB_NAME) and restore_db_cache(name):
         return
     create_db()
     with Transaction().start(DB_NAME, 1, close=True) as transaction:
@@ -81,7 +78,7 @@ def activate_module(name, cache_file_name=None):
             ActivateUpgrade(instance_id).transition_upgrade()
             ActivateUpgrade.delete(instance_id)
             transaction.commit()
-    backup_db_cache(cache_file_name)
+    backup_db_cache(name)
 
 
 def restore_db_cache(name):
@@ -212,12 +209,11 @@ def with_transaction(user=1, context=None):
 class ModuleTestCase(unittest.TestCase):
     'Trytond Test Case'
     module = None
-    cache_file_name = None
 
     @classmethod
     def setUpClass(cls):
         drop_db()
-        activate_module(cls.module, cls.cache_file_name)
+        activate_module(cls.module)
         super(ModuleTestCase, cls).setUpClass()
 
     @classmethod
