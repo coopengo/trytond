e99022d3037c6162b5165f47654660909f8f05e7
PierreCookie <pkunkel@sfr.fr>
2018-07-03 16:33:10 +0200

    Restore original tools.py
    Fix #9220

--- trytond/tests/tools.py
+++ trytond/tests/tools.py
@@ -2,21 +2,19 @@
 # this repository contains the full copyright notices and license terms.
 from proteus import config as pconfig, Model, Wizard
 
-from .test_tryton import restore_db_cache, backup_db_cache, drop_db, create_db
+from .test_tryton import restore_db_cache, backup_db_cache, drop_create
 
 __all__ = ['activate_modules', 'set_user']
 
 
-# JCA : Allow to force filename in case it is too long
-def activate_modules(modules, cache_file_name=None):
+def activate_modules(modules):
     if isinstance(modules, basestring):
         modules = [modules]
-    cache_name = cache_file_name or '-'.join(modules)
-    # JCA : restore_db_cache fails if the database already exists
-    drop_db()
+    cache_name = '-'.join(modules)
     if restore_db_cache(cache_name):
         return _get_config()
-    create_db()
+    drop_create()
+
     cfg = _get_config()
     Module = Model.get('ir.module')
     records = Module.find([
