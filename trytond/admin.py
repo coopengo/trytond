65e685dd7b571d7ef8bdf2249aea33a7ef09c4ca
Xun GONG <xun.gong@coopengo.com>
2019-12-19 10:58:20 +0100

    Dev on #13864: Upgrade only when necessary with new command line

--- trytond/admin.py
+++ trytond/admin.py
@@ -12,8 +12,35 @@ from trytond import backend
 from trytond.pool import Pool
 from trytond.config import config
 
+from trytond.modules import get_module_info, get_module_list
+from trytond.cache import Cache
+
 __all__ = ['run']
 logger = logging.getLogger(__name__)
+
+def _check_update_needed(db_name, options, transaction):
+    # Get current main module version
+    main_module = config.get('version', 'module', default='coog_core')
+    current_main_module_version = get_module_info(main_module)['version']
+
+    # Do the upgrade anyway if -u is activated
+    if options.update:
+        return True, current_main_module_version
+
+    # Get main module version which stocked in the database
+    version_control_table = Table('upgrade_version_control')
+    cursor = transaction.connection.cursor()
+    cursor.execute(*version_control_table.select(version_control_table.current_version, version_control_table.is_upgrading))
+    db_main_module_version, is_upgrading = cursor.fetchone()
+
+    # If the main module is upgrading, other upgradings are not allowed
+    if is_upgrading:
+        return False, current_main_module_version
+
+    if options.check_update and current_main_module_version != db_main_module_version:
+        return True, current_main_module_version
+
+    return False, current_main_module_version
 
 
 def run(options):
@@ -47,18 +74,39 @@ def run(options):
             lang.add(main_lang)
         else:
             lang = set()
+
         lang |= set(options.languages)
         pool = Pool(db_name)
-        pool.init(update=options.update, lang=list(lang),
-            activatedeps=options.activatedeps)
 
-        if options.update_modules_list:
-            with Transaction().start(db_name, 0) as transaction:
+        # Do the update only when it is needed
+        with Transaction().start(db_name, 0) as transaction:
+            cursor = transaction.connection.cursor()
+            # Lock table to upgrade
+            cursor.execute("LOCK upgrade_version_control IN EXCLUSIVE MODE;")
+            is_upgrade_needed, new_version = _check_update_needed(db_name, options, transaction)
+            if not is_upgrade_needed:
+                options.update = []
+                options.check_update = []
+            pool.init(update=options.update or options.check_update, lang=list(lang), activatedeps=options.activatedeps)                
+            if is_upgrade_needed:
+                # If upgrade finishes correctly -> update version in database and reset is_upgrading to false
+                try:
+                    version_control_table = Table('upgrade_version_control')
+                    cursor.execute(*version_control_table.update(
+                        columns=[version_control_table.current_version, version_control_table.is_upgrading],
+                        values=[new_version, False]))
+                    transaction.commit()
+
+                except:
+                    transaction.rollback()
+                    logger.info('Upgrade was interrupted!')
+                    raise
+
+            if options.update_modules_list:
                 Module = pool.get('ir.module')
                 Module.update_list()
 
-        if lang:
-            with Transaction().start(db_name, 0) as transaction:
+            if lang:
                 pool = Pool()
                 Lang = pool.get('ir.lang')
                 languages = Lang.search([
