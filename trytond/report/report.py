71e171683779f62d6527056793e6971f10413f05
jmousset <jeremy.mousset@coopengo.com>
2020-02-10 13:43:10 +0100

    report: add 2 retries by default on calls to unconv api (#96)
    This is a workaround to
    Fix #10833
    while waiting for fix on #14733

--- trytond/report/report.py
+++ trytond/report/report.py
@@ -33,6 +33,7 @@ from trytond.url import URLMixin
 from trytond.rpc import RPC
 from trytond.exceptions import UserError
 
+
 logger = logging.getLogger(__name__)
 
 import requests
@@ -70,6 +71,9 @@ FORMAT2EXT = {
     'xlsx': 'xlsx',
     }
 
+
+class UnoConversionError(UserError):
+    pass
 
 class ReportFactory:
 
@@ -372,11 +376,18 @@ class Report(URLMixin, PoolBase):
         url_tpl = config.get('report', 'api')
         url = url_tpl.format(oext=oext)
         files = {'file': ('doc.' + input_format, data)}
-        r = requests.post(url, files=files)
-        if r.status_code < 300:
-            return oext, r.content
-        else:
-            raise Exception(r)
+        for count in range(config.getint('report', 'unoconv_retry'), -1, -1):
+            try:
+                r = requests.post(url, files=files)
+                if r.status_code < 300:
+                    return oext, r.content
+                else:
+                    raise UnoConversionError(r)
+            except UnoConversionError:
+                if count:
+                    time.sleep(0.1)
+                    continue
+                raise
 
     @classmethod
     def format_date(cls, value, lang=None):
