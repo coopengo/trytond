32979ac226b21ee831153ab7e8aa33d423a60d46
Nicolas Évrard <nicoe@users.noreply.github.com>
2020-10-23 13:56:58 +0200

    [2.8] Ensure dependencies of fields are also loaded on Form display (#108)

--- trytond/model/fields/field.py
+++ trytond/model/fields/field.py
@@ -428,6 +428,7 @@ class Field(object):
             'context': encoder.encode(self.context),
             'loading': self.loading,
             'name': self.name,
+            'depends': self.depends,
             'on_change': list(self.on_change),
             'on_change_with': list(self.on_change_with),
             'readonly': self.readonly,
