cd0b0c297729bbd5fa4e0b2898dcb1fdd1dfd546
jmousset <jeremy.mousset@coopengo.com>
2020-11-26 18:14:46 +0100

    report: make error response of unoconv json serializable (#116)
    * report: make error response of unoconv json serializable
    
    * log user
    
    * handle no name

--- trytond/report/report.py
+++ trytond/report/report.py
@@ -7,7 +7,6 @@ import inspect
 import logging
 import subprocess
 import tempfile
-import time
 import warnings
 import zipfile
 import requests
@@ -395,6 +394,7 @@ class Report(URLMixin, PoolBase):
     @classmethod
     def convert_api(cls, report, data, timeout):
         # AKE: support printing via external api
+        User = Pool().get('res.user')
         input_format = report.template_extension
         output_format = report.extension or report.template_extension
 
@@ -411,11 +411,15 @@ class Report(URLMixin, PoolBase):
                 if r.status_code < 300:
                     return oext, r.content
                 else:
-                    raise UnoConversionError(r)
-            except UnoConversionError:
+                    raise UnoConversionError('Conversion of "%s" failed. '
+                        'Unoconv responsed with "%s".' % (
+                            report.report_name, r.reason))
+            except UnoConversionError as e:
                 if count:
                     time.sleep(0.1)
                     continue
+                user = User(Transaction().user)
+                logger.error(e.message + ' User: %s' % user.name or '')
                 raise
 
     @classmethod
