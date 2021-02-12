ccd8eb1fb564a193e67539c38e2fb706825e2b90
Jean Cavallo <jean.cavallo@hotmail.fr>
2021-02-12 11:36:49 +0100

    [2.10] Properly clear cache listeners during tests, disable autocache when upgrading (#131)

--- trytond/cache.py
+++ trytond/cache.py
@@ -331,6 +331,16 @@ class MemoryCache(BaseCache):
                 if cls._listener.get((pid, dbname)) == current_thread:
                     del cls._listener[pid, dbname]
 
+    @classmethod
+    def purge_listeners(cls, dbname):
+        '''
+        Purges all listeners for a given database
+        '''
+        pid = os.getpid()
+        with cls._listener_lock[pid]:
+            if (pid, dbname) in cls._listener:
+                del cls._listener[pid, dbname]
+
 
 class DefaultCacheValue:
     pass
