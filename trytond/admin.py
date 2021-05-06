f75036de6878f9847b629d03fe7d57fcedd22092
Jean Cavallo <jean.cavallo@hotmail.fr>
2021-05-06 13:45:05 +0200

    Properly check for checkupdate [CUSTOM]

--- trytond/admin.py
+++ trytond/admin.py
@@ -117,7 +117,7 @@ def run(options):
         init[db_name] = False
         database = backend.Database(db_name)
         database.connect()
-        if options.update:
+        if options.update or options.check_update:
             if not database.test():
                 logger.info("init db")
                 database.init()
