edc4cc720e5d6aeede8e0924db5918467d3fdcf4
Nicolas Évrard <nicoe@users.noreply.github.com>
2022-06-13 10:08:46 +0200

    Handle absence of connection in Cache._listen (#199)

--- trytond/cache.py
+++ trytond/cache.py
@@ -362,10 +362,11 @@ class MemoryCache(BaseCache):
             raise NotImplementedError
 
         logger.info("listening on channel '%s' of '%s'", cls._channel, dbname)
-        conn = database.get_connection(autocommit=True)
         pid = os.getpid()
         current_thread = threading.current_thread()
+        conn = None
         try:
+            conn = database.get_connection(autocommit=True)
             cursor = conn.cursor()
             cursor.execute('LISTEN "%s"' % cls._channel)
             current_thread.listening = True
@@ -397,7 +398,8 @@ class MemoryCache(BaseCache):
                 "cache listener on '%s' crashed", dbname, exc_info=True)
             raise
         finally:
-            database.put_connection(conn)
+            if conn:
+                database.put_connection(conn)
             with cls._listener_lock[pid]:
                 if cls._listener.get((pid, dbname)) == current_thread:
                     del cls._listener[pid, dbname]
