c09dde132be70d9a5ef25511c6575653d67cbe29
Jean Cavallo <jean.cavallo@hotmail.fr>
2022-07-18 13:21:16 +0200

    tests: [2.12] Harden test ending (#200)
    Fix #23556

--- trytond/cache.py
+++ trytond/cache.py
@@ -1,6 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import datetime as dt
+import time
 import json
 import logging
 import os
@@ -334,9 +335,10 @@ class MemoryCache(BaseCache):
                                 inst._clear(dbname)
                 cls._clean_last = datetime.now()
         except Exception:
-            logger.error(
-                "cache listener on '%s' crashed", dbname, exc_info=True)
-            raise
+            if not config.getboolean('env', 'testing'):
+                logger.error(
+                    "cache listener on '%s' crashed", dbname, exc_info=True)
+                raise
         finally:
             if conn:
                 database.put_connection(conn)
@@ -345,14 +347,28 @@ class MemoryCache(BaseCache):
                     del cls._listener[pid, dbname]
 
     @classmethod
-    def purge_listeners(cls, dbname):
+    def _purge_listeners(cls, dbname):
         '''
         Purges all listeners for a given database
+
+        Should no longer be useful, but we may need it later so we will keep it
+        around a little longer
         '''
         pid = os.getpid()
+        thread_id = None
         with cls._listener_lock[pid]:
             if (pid, dbname) in cls._listener:
+                thread_id = cls._listener[pid, dbname].ident
                 del cls._listener[pid, dbname]
+
+        # JMO : doctest teardown remains stuck with code below
+        # TODO: fix this
+        if config.getboolean('env', 'testing'):
+            # We removed the thread from the list, but it can still be alive if
+            # it is busy clearing some cache
+            if thread_id is not None:
+                while {thread_id} & {x.ident for x in threading.enumerate()}:
+                    time.sleep(0.01)
 
 
 class DefaultCacheValue:
