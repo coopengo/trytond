bb18362907f2bc13741f444b0872264d1a85b005
Nicolas Evrard <nicoe@openhex.org>
2023-03-03 17:42:08 +0100

    Keep TableHandler signature from previous version

--- trytond/backend/table.py
+++ trytond/backend/table.py
@@ -13,7 +13,7 @@ class TableHandlerInterface(object):
     namedatalen = None
     __handlers = WeakKeyDictionary()
 
-    def __new__(cls, model, history=False):
+    def __new__(cls, model, module_name=None, history=False):
         transaction = Transaction()
         handlers = cls.__handlers.setdefault(transaction, {})
         key = (model.__name__, history)
